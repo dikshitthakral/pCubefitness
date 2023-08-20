1624618b5f4cce4d14d15775c8c937a289749df8
dikshitthakral <apple@apples-MacBook-Pro.local>
2023-08-21 00:11:25 +0530

    Added build

---
+++ build/static/js/485.f5af693b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgpt=self.webpackChunkgpt||[]).push([[485],{2604:function(n,s,t){t.r(s),t.d(s,{default:function(){return r}});var a=t(9476),i=(t(2183),t(184)),c=function(){return(0,i.jsx)("div",{className:"".concat(a.Z.first," margin-sections main-background"),"data-aos":"zoom-out-down",children:(0,i.jsx)("h1",{className:"main-heading",children:"Our Studios"})})},e=(t.p,t(9800)),r=function(){return(0,i.jsxs)("div",{children:[(0,i.jsx)(c,{}),(0,i.jsx)(e.Z,{})]})}}}]);
+//# sourceMappingURL=485.f5af693b.chunk.js.map
