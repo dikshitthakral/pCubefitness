73d0882748973e51a66dd08c1429303e54d6ebb1
dikshitthakral <apple@apples-MacBook-Pro.local>
2023-09-02 21:31:05 +0530

    Updated alignment issues and other remaining issues

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Home from "./components/home/Home";
 import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 import Second from "./components/trainersPage/Second";
-
+const Home = React.lazy(() =>
+  import("./components/home/Home")
+);
 const About = React.lazy(() => import("./components/about/About"));
 const TrainersPage = React.lazy(() =>
   import("./components/trainersPage/TrainersPage")
 );
 
 const Contact = React.lazy(() => import("./components/contact/Contact"));
-
-
 
 const App = () => {
   return (
@@ -24,15 +23,14 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
-             {/* <Route path="trainers" element={<TrainersPage />} /> */}
           <Route path="studios" element={<TrainersPage/>} />
         <Route path="contact" element={<Contact />} />
        
-          {/* <Route path="calculator" element={<Calculator />} /> */}
          
         </Routes>
+        
       </Suspense>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
